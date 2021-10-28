17c8f636867a986fb36da25e528f1cbdc2537a5e
ddorosh1 <doroshdmitriy@gmail.com>
2021-10-28 16:51:35 -0700

    Added conditional rendering

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -6,12 +6,16 @@ const UserForm = props => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+    const [passwordError, setPasswordError] = useState('');
 
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = { firstName, lastName, email, password };
+        const newUser = { firstName, lastName, email, password, confirmPassword };
         console.log(newUser);
-        console.log(`I'm creating a user`)
+        if(newUser.password != newUser.confirmPassword) {
+            setPasswordError(`Passwords don't match.`);
+        }
         setFirstName("");
         setLastName("");
         setEmail("");
@@ -19,9 +23,17 @@ const UserForm = props => {
         setConfirmPassword("");
     };
 
+
+
     return(
         <div>
             <form onSubmit={ createUser }>
+                    {/* ternary operator */}
+                {
+                    hasBeenSubmitted ? 
+                    <h3>Thank you for submitting the form!</h3> :
+                    <h3>Welcome, please submit the form.</h3> 
+                }
                 <div>
                     <label>First Name: </label>
                     <input type="text" onChange= { (e) => setFirstName(e.target.value) } value={ firstName } />
@@ -32,17 +44,22 @@ const UserForm = props => {
                 </div>
                 <div>
                     <label>Email: </label>
-                    <input type="text" onChange = { (e) => setEmail(e.target.value) } value={ email } />
+                    <input type="email" onChange = { (e) => setEmail(e.target.value) } value={ email } />
                 </div>
                 <div>
                     <label>Password: </label>
                     <input type="password" name="password" id="password" onChange = { (e) => setPassword(e.target.value) } value={ password } />
                 </div>
                 <div>
+                    {
+                        passwordError ?
+                        <p style={{color:'red'}}>{ passwordError }</p> :
+                        ''
+                    }
                     <label>Confrim Password: </label>
                     <input type="password" name="confirmPassword" id="confirmPassword" onChange= { (e) => setConfirmPassword(e.target.value) } value={ confirmPassword } />
                 </div>
-                <button type="submit">Submit</button>
+                <input type="submit" value="Create User" onClick = { setHasBeenSubmitted } />
             </form>
             <div>
                 <h2>First Name: { firstName } </h2>
